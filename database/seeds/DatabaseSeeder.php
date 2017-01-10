e8fda07d6e8868a668d7286b14749cbfe200b22c
Esteban Sotillo <esteban.sotillo@cpnv.ch>
2017-01-10 14:29:45 +0100

    Adding seed for a default admin login

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,25 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserTableSeeder::class);
+      DB::table('members')->insert([
+         'last_name' => 'admin',
+         'first_name' => 'admin',
+         'address' => 'CPNV',
+         'zip_code' => '1450',
+         'city' => 'CPNV',
+         'email' => env('ADMIN_MAIL','admin'),
+         'mobile_phone'=>'000000000',
+         'home_phone'=>'000000000',
+         'birth_date'=>'1980-01-01',
+         'password' => bcrypt(env('ADMIN_PASS','admin')),
+         'login' => env('ADMIN_LOGIN','admin'),
+         'token' => '',
+         'active' => '1',
+         'to_verify' => '0',
+         'validate' => '1',
+         'administrator' => '1',
+         'created_at' => '2017-01-10 13:58:14',
+         'updated_at' => '2017-01-10 13:58:14'
+       ]);
     }
 }
