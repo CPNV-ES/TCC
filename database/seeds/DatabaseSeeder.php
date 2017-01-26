8209e8b932f2a35ff4f9f065ebef02961877c78f
ilias <igoujgali@gmail.com>
2017-01-26 09:02:39 +0100

    added seed
    -fake user

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -31,31 +31,39 @@ class DatabaseSeeder extends Seeder
          'created_at' => '2017-01-10 13:58:14',
          'updated_at' => '2017-01-10 13:58:14'
        ]);
-      // A REVOIR Essayer de prendre des valeurs réaliste
-//      for($i=1;$i<10;$i++)
-//      {
-//
-//          DB::table('members')->insert([
-//              'last_name' => 'test'.$i,
-//              'first_name' => 'Nom'.$i,
-//              'address' => 'CPNV',
-//              'zip_code' => '1450',
-//              'city' => 'CPNV',
-//              'email' => 'test.test'.$i.'@test.dev',
-//              'mobile_phone'=>'000000000',
-//              'home_phone'=>'000000000',
-//              'birth_date'=>'1980-01-01',
-//              'password' => 'test',
-//              'login' => 'test'.$i,
-//              'token' => '',
-//              'active' => $i%2,
-//              'to_verify' => '0',
-//              'validate' => '1',
-//              'administrator' => '0',
-//              'created_at' => '2017-01-10 13:58:14',
-//              'updated_at' => '2017-01-10 13:58:14'
-//          ]);
-//      }
+        $first_name = ['Franklin','Micheal','Joesph','Jérémie','Christian', 'Gabrielle','Kim','Tiffany','Lee','Emilie'];
+        $last_name = ['Favre','Jeanmonot','De la Gouj', 'L\'Eplattenier','Gaille','Major','Adma','Rizz','Bergerman','Bakerman'];
+
+        $address = ['Rue de la gare','Rue de la boulangerie','Rue des plalet','Route de l\'orge', 'route des pèlerin', 'route du château'];
+        $locality = ['Provence', 'Ste-Croix','Vuitevoeuf','Baumes','Charvonnay','Yverdon'];
+        for($i=1;$i<10;$i++)
+        {
+            $lName=$last_name[mt_rand(0,sizeof($last_name)-1)];
+            $fName=$first_name[mt_rand(0,sizeof($first_name)-1)];
+            $addr = $address[mt_rand(0,sizeof($address)-1)];
+            $local = $locality[mt_rand(0,sizeof($address)-1)];
+            DB::table('members')->insert([
+                'last_name' => $lName,
+                'first_name' => $fName,
+                'address' => $addr,
+                'zip_code' => mt_rand(1000,9999),
+                'city' => $local,
+                'email' => $lName.'.'.$fName.$i.'@test.dev',
+                'mobile_phone'=>mt_rand(1000000000,9999999999),
+                'home_phone'=>mt_rand(1000000000,9999999999),
+                'birth_date'=>date("Y-M-d",mt_rand('1',time()-72*30)),//'1980-01-01',
+                'password' => bcrypt('test'),
+                'login' => $fName.$i,
+                'token' => '',
+                'active' => mt_rand(0,1),
+                'to_verify' => mt_rand(0,1),
+                'validate' => mt_rand(0,1),
+                'administrator' => '0',
+                'created_at' => '2017-01-10 13:58:14',
+                'updated_at' => '2017-01-10 13:58:14'
+            ]);
+        }
+
        DB::table('subscriptions')->insert([
           'status' => 'member',
           'amount' => '10',
