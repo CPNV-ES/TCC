97e1e68786cf373326b03965f8543356ed7555b8
LogLauncher <struan.forsyth@cpnv.ch>
2017-02-10 10:23:35 +0100

    Fix court list, add & edit

--- app/Court.php
+++ app/Court.php
@@ -7,6 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Court extends Model
 {
 
+    protected $fillable = [
+        'name',
+        'state',
+        'nbDays'
+    ];
+
     public function reservations() {
         return $this->hasMany('App\Reservation', 'fkCourt');
     }
