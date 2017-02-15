1d807d03cfdea306f7a7c8098d9f9d4c52180c3a
Unknown <westixy@gmail.com>
2017-02-15 16:04:52 +0100

    ...

--- public/js/ee.js
+++ public/js/ee.js
@@ -1,6 +1,9 @@
-window.addEventListener('load', function(e) {
-  var r=Math.floor(Math.random() * 2000);
+function ee(r){
   var c=document.querySelector('.background');
   var imgs=['/css/images/ee/1.jpg','/css/images/ee/2.gif','/css/images/ee/3.jpg'];
   if(r==0) if(c!=null) c.style.backgroundImage='url('+imgs[Math.floor(Math.random() * imgs.length)]+')';
+}
+window.addEventListener('load', function(e) {
+  var r=Math.floor(Math.random() * 2000);
+  ee(r);
 });
