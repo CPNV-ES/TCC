8a1f047a15e09221cbee49e7fd5ae6302601027a
ilias <igoujgali@gmail.com>
2017-03-03 15:27:19 +0100

    modified config seed
    added field currentAmount
    added field nbReservations

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -165,7 +165,8 @@ class DatabaseSeeder extends Seeder
             'nbDaysLimitNonMember' => 5,
             'courtOpenTime' => '08:00:00',
             'courtCloseTime' => '17:00:00',
-            'currentAmount' => 20.00
+            'currentAmount' => 20.00,
+            'nbReservations' => 3
         ]);
 
         // *** RESERVATIONS ***
