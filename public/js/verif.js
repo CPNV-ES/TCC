a4ab34a40f132dbde5687564acef64edd088acfc
ilias <igoujgali@gmail.com>
2017-03-10 15:09:30 +0100

    stay on the used court when reservation  is done/failed

--- public/js/verif.js
+++ public/js/verif.js
@@ -5,6 +5,7 @@
  */
  'use strict';
  if(Array.prototype.forEach==undefined) Array.prototype.forEach=function(cb){for(var i=0;i<this.length;i++){cb(this[i],i);}}
+ if(NodeList.prototype.forEach==undefined) NodeList.prototype.forEach=Array.prototype.forEach;
  
 
  var VERIF = {
