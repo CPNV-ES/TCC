6d3b433eb6904dea2530561ab1369a4b36d97363
ilias <igoujgali@gmail.com>
2017-05-22 14:28:00 +0200

    delete duplicate id for delete reservation modal title

--- resources/views/booking/delete_modal.blade.php
+++ resources/views/booking/delete_modal.blade.php
@@ -4,7 +4,7 @@
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                <h4 class="modal-title" id="myModalLabel">Supprimer réservation</h4>
+                <h4 class="modal-title" >Supprimer réservation</h4>
             </div>
             <div class="modal-body">
                 <div id="modal-del-resume" class="notice notice-info">
