21736de5c32edee29b5c30b48ea9a1c1ede59f98
Esteban Sotillo <Westixy@users.noreply.github.com>
2017-06-19 09:07:13 +0200

    Update tag info

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -122,7 +122,7 @@
 
         <footer style="position:absolute;">
           <p>© Centre Professionnel du Nord Vaudois / 2016 - {{Date('Y')}}</p>
-          <p id="GIT_RELEASE" style="position:fixed;bottom: 0px;font-size: 10px;right:15px;color:rgba(0,0,0,.2);">V1.0.0</p>
+          <p id="GIT_RELEASE" style="position:fixed;bottom: 0px;font-size: 10px;right:15px;color:rgba(0,0,0,.2);">V1.0.1</p>
         </footer>
         <script>
         // Funtion for the resize of the body.
